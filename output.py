d91a85a2b3453b8aa20a6effd33e0384a2ad0e15
Erick <erickmwenda317@gmail.com>
2024-11-22 12:55:54 +0300

    Create output.py

---
+++ output.py
@@ -0,0 +1,7 @@
+iPhone 14 is now turned on.
+iPhone 14 is ready for use, welcome!
+Calling 123-456-7890 from iPhone 14...
+Sending message to 123-456-7890: Hello!
+Charging... Battery at 70%
+Battery percentage: 70%
+iPhone 14 is now turned off.
